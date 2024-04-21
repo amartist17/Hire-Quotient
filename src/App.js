583827e93a26644e342d913ae8bc66e7bb091bbf
Amar Singh <gujraal2006@gmail.com>
2024-04-21 12:31:05 +0530

    style changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import HoldingsTable from './components/HoldingsTable';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Open Sans',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+  },
+});
 function App() {
   return (
     <div>
       <HoldingsTable />
+      <footer style={{ textAlign: 'center', marginTop: '1rem' }}>Submitted by <a href="https://amartist.tech/">Amardeep Singh</a></footer>
     </div>
   );
 }
